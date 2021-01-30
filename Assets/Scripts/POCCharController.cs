25970f4d3b9419378a9165814cd5c12006da051e
Rowan <rowan@localhostlimited.com>
2021-01-30 01:32:05 +0000

    Move low poly pack into folder; added POC conveyor tile assets; added example tilemap with POC collisions to Rowan-POCScene;

---
+++ Assets/Scripts/POCCharController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class POCCharController : MonoBehaviour
+{
+    public float acceleration = 5f;
+    public float maxVelocity = 5f;
+
+    private Rigidbody2D _rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float accelerationX = Input.GetAxis("Horizontal") * acceleration;
+        float accelerationY = Input.GetAxis("Vertical") * acceleration;
+
+        accelerationX *= Time.deltaTime;
+        accelerationY *= Time.deltaTime;
+
+        // Update rigidbody velocity
+        _rigidbody.velocity += new Vector2(accelerationX, accelerationY);
+    }
+
+    void FixedUpdate()
+    {
+        _rigidbody.velocity = Vector2.ClampMagnitude(_rigidbody.velocity, maxVelocity);
+    }
+}
