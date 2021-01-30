4a5412d34b5b3cf030e406601e3c930cc42249d2
JRobbins93 <jrobbins@live.co.uk>
2021-01-30 11:26:03 +0000

    Combining player movement scripts. Prefabbing player character.

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private SpriteRenderer Sprite;
+
+    void Start()
+    {
+        Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnPickedUp()
+    {
+        Sprite.enabled = false;
+    }
+
+    public void OnPutDown()
+    {
+        Sprite.enabled = true;
+    }
+}
---
+++ Assets/Scripts/PlayerCharacterInteraction.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterInteraction : MonoBehaviour
+{
+    private BoxCollider2D BoxTrigger;
+
+    private Item HeldItem;
+
+    private Vector3 ItemDropOffset;
+
+    void Start()
+    {
+        BoxTrigger = GetComponent<BoxCollider2D>();
+        ItemDropOffset = BoxTrigger.offset;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if (HeldItem == null)
+            {
+                List<Collider2D> itemsInRange = new List<Collider2D>();
+                int itemsInRangeCount = BoxTrigger.OverlapCollider(new ContactFilter2D(), itemsInRange);
+
+                Debug.Assert(itemsInRangeCount <= 1);
+
+                if (itemsInRangeCount == 1)
+                {
+                    Item itemInRange = itemsInRange[0].GetComponent<Item>();
+                    HeldItem = itemInRange;
+                    itemInRange.OnPickedUp();
+                }
+            }
+            else
+            {
+                HeldItem.transform.position = transform.position + transform.rotation * ItemDropOffset;
+                HeldItem.OnPutDown();
+                HeldItem = null;
+            }
+        }
+    }
+}
