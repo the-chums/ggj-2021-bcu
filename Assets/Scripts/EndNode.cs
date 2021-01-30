858628d49811cbb9fe1a0260c878e9d1380932de
JRobbins93 <jrobbins@live.co.uk>
2021-01-30 22:46:20 +0000

    Responding to items reaching end of conveyor

--- Assets/Scripts/EndNode.cs
+++ Assets/Scripts/EndNode.cs
@@ -6,11 +6,14 @@ public class EndNode : MonoBehaviour
     public ItemColour ValidColor;
 
     private SpriteRenderer _renderer;
+
+    private WinLossTracker WinLossTracker;
     
     // Start is called before the first frame update
     void Start()
     {
         _renderer = GetComponent<SpriteRenderer>();
+        WinLossTracker = FindObjectOfType<WinLossTracker>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,24 @@ public class EndNode : MonoBehaviour
         {
             ColorUtility.TryParseHtmlString($"#{ValidColor.Hex}", out Color color);
             _renderer.color = color;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Item item = collision.GetComponent<Item>();
+        if(item != null)
+        {
+            if(item.Colour == ValidColor)
+            {
+                WinLossTracker.OnSuccess();
+            }
+            else
+            {
+                WinLossTracker.OnFailure();
+            }
+
+            item.gameObject.SetActive(false);
         }
     }
 }
