7ba5a2b6448bb06352a1abd15df894595a9892d4
JRobbins93 <jrobbins@live.co.uk>
2021-01-30 14:49:17 +0000

    Adding menu screens

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnNewGameClicked()
+    {
+        SceneManager.LoadScene("Rowan-POCScene");
+    }
+
+    public void OnCreditsClicked()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
