8a76bed6ef3c6f4fecd996434cd37a3d5dc80446
Rowan <rowan@localhostlimited.com>
2021-01-30 21:02:01 +0000

    End node mvp

---
+++ Assets/Scripts/EndNode.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class EndNode : MonoBehaviour
+{
+    public ItemColour ValidColor;
+
+    private SpriteRenderer _renderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnValidate()
+    {
+        if (ValidColor)
+        {
+            ColorUtility.TryParseHtmlString($"#{ValidColor.Hex}", out Color color);
+            _renderer.color = color;
+        }
+    }
+}
