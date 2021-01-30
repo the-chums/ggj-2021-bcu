93a5935c7e71149ea5b683e1544b47d33002d8da
Rowan <rowan@localhostlimited.com>
2021-01-30 11:28:46 +0000

    Test of directional conveyor tiles, add conveyor tile lookup interface

---
+++ Assets/Scripts/ConveyorTileManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class ConveyorTileManager : MonoBehaviour, IConveyorTileLookup
+{
+    public Tilemap ConveyorTilemap;
+
+    void Start()
+    {
+        Tilemap tilemap = ConveyorTilemap;
+
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] allTiles = tilemap.GetTilesBlock(bounds);
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = allTiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
+                }
+                else
+                {
+                    Debug.Log("x:" + x + " y:" + y + " tile: (null)");
+                }
+            }
+        }
+    }
+
+    public string GetActiveTile(Vector2Int position)
+    {
+        return null;
+    }
+
+    public string GetCardinalTile(Vector2Int position, IConveyorTileLookup.Direction direction)
+    {
+        return null;
+    }
+}
---
+++ Assets/Scripts/IConveyorTileManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public interface IConveyorTileLookup
+{
+    public enum Direction
+    {
+        Up,
+        Right,
+        Down,
+        Left
+    };
+
+    // Given a point, what are the tiles in the 4 cardinal directions and itself
+    string GetActiveTile(Vector2Int position);
+
+    // Map position and direction to an action:
+    // Template: movement_tblr-movement_tblr
+    string GetCardinalTile(Vector2Int position, Direction direction);
+}
