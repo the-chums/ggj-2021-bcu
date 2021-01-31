b7a40e83a8d2061fec4b78c01ebe21a88f215442
JRobbins93 <jrobbins@live.co.uk>
2021-01-31 13:38:57 +0000

    Adding ftue prompts

---
+++ Assets/Scripts/FTUEPrompt.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FTUEPrompt : MonoBehaviour
+{
+    private PlayerCharacterMovement CharacterMovement;
+
+    private void Start()
+    {
+        CharacterMovement = FindObjectOfType<PlayerCharacterMovement>();
+        Debug.Assert(CharacterMovement);
+
+        CharacterMovement.enabled = false;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Escape))
+        {
+            CharacterMovement.enabled = true;
+            gameObject.SetActive(false);
+            enabled = false;
+        }
+    }
+}
