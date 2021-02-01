884a8e57888c353edfea2abcb552087219f60ef1
SeanTheMighty <seanthemighty@me.com>
2021-02-01 20:03:22 +0000

    Slight adjustments to level logic for levels 1-7, added pause prefab to levels, added deceleration lerp to control character deceleration

--- Assets/Scripts/PlayerCharacterMovement.cs
+++ Assets/Scripts/PlayerCharacterMovement.cs
@@ -7,55 +7,62 @@ using Vector2 = UnityEngine.Vector2;
 
 public class PlayerCharacterMovement : MonoBehaviour
 {
-    public float Acceleration = 15f;
-    public float MaxVelocity = 6f;
-    public float MaxTimeToSpeed = 0.2f;
-    
-    private Rigidbody2D RigidBody;
+    public float Acceleration = 25f;
+    public float MaxVelocity = 8f;
+    public float MaxTimeToSpeed = 0.15f;
+    public float decelerationSpeed = 15f;
+
+    private Rigidbody2D rb2d;
 
     void Start()
     {
-        RigidBody = GetComponent<Rigidbody2D>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        float accelerationDelta = Acceleration / MaxTimeToSpeed * Time.deltaTime;
-
-        float accelerationX = horizontalInput * accelerationDelta;
-        float accelerationY = verticalInput * accelerationDelta;
-        Vector2 acceleration = new Vector2(accelerationX, accelerationY);
-
-        RigidBody.velocity += acceleration;
-
-        float zRotation = transform.eulerAngles.z;
-
-        if (horizontalInput > 0)
+        if (Input.GetAxisRaw("Horizontal") != 0f || Input.GetAxisRaw("Vertical") != 0f)
         {
-            zRotation = -90;
-        }
-        else if(horizontalInput < 0)
-        {
-            zRotation = 90;
-        }
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
+            float accelerationDelta = Acceleration / MaxTimeToSpeed * Time.deltaTime;
 
-        if (verticalInput > 0)
-        {
-            zRotation = 0;
-        }
-        else if (verticalInput < 0)
-        {
-            zRotation = 180;
-        }
+            float accelerationX = horizontalInput * accelerationDelta;
+            float accelerationY = verticalInput * accelerationDelta;
+            Vector2 acceleration = new Vector2(accelerationX, accelerationY);
 
-        transform.rotation = UnityEngine.Quaternion.Euler(0, 0, zRotation);
+            rb2d.velocity += acceleration;
+
+            float zRotation = transform.eulerAngles.z;
+
+            if (horizontalInput > 0)
+            {
+                zRotation = -90;
+            }
+            else if (horizontalInput < 0)
+            {
+                zRotation = 90;
+            }
+
+            if (verticalInput > 0)
+            {
+                zRotation = 0;
+            }
+            else if (verticalInput < 0)
+            {
+                zRotation = 180;
+            }
+
+            transform.rotation = UnityEngine.Quaternion.Euler(0, 0, zRotation);
+        }
+        else
+        {
+            rb2d.velocity = Vector2.Lerp(rb2d.velocity, Vector2.zero, decelerationSpeed * Time.deltaTime);
+        }
     }
 
     void FixedUpdate()
     {
-        RigidBody.velocity = Vector2.ClampMagnitude(RigidBody.velocity, MaxVelocity);
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, MaxVelocity);
     }
 }
