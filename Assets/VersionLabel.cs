2edb7c903992a12830c4dc34215db8f680de2947
Rowan <rowan@localhostlimited.com>
2021-02-03 17:25:41 +0000

    Add version label; new build;

---
+++ Assets/VersionLabel.cs
@@ -0,0 +1,11 @@
+﻿using TMPro;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class VersionLabel : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<TMP_Text>().text = $"v{Application.version}";
+    }
+}
