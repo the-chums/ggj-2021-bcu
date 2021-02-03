b48b07e28596065e77b590f3d1fe56e3e6c03a30
Rowan <rowan@localhostlimited.com>
2021-02-03 16:51:05 +0000

    add level label onto hud; set hud prefab to be used in every level; rebuilt webgl;

---
+++ Assets/Scripts/LevelLabel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class LevelLabel : MonoBehaviour
+{
+    void Start()
+    {
+        GetComponent<Text>().text = $"LVL {SceneManager.GetActiveScene().name}";
+    }
+}
--- Assets/Scripts/WinLossTracker.cs
+++ Assets/Scripts/WinLossTracker.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.Analytics;
-using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+using UnityEngine.SceneManagement;
 
 public class WinLossTracker : MonoBehaviour
 {
